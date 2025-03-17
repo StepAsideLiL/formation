ff55ca239221904da7ad1641916e5b556aeda5e5
Rifat Khan <rifat6633@gmail.com>
2025-03-17 21:09:23 +0600

    feat: add auth route and clientAuth.

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,9 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prismadb";
+import { createAuthClient } from "better-auth/react";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "cockroachdb",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
+
+export const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
 });
