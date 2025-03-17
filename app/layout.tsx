7d87adf26e23030ced4bda32c72f839728093863
Rifat Khan <rifat6633@gmail.com>
2025-03-17 22:42:52 +0600

    feat: add site icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,12 @@ import fonts from "@/lib/fonts";
 export const metadata: Metadata = {
   title: "Formation",
   description: "Create form to collect information",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `/site.webmanifest`,
 };
 
 export default function RootLayout({
