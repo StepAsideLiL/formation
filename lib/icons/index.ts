89d01e19a55842730dc5cbfc453166878a95c8b2
Rifat Khan <rifat6633@gmail.com>
2025-03-20 16:46:24 +0600

    chore: add new icons.

--- lib/icons/index.ts
+++ lib/icons/index.ts
@@ -1,7 +1,12 @@
 import * as L from "lucide-react";
 
 const Icons = {
+  Checkbox: L.SquareCheck,
+  Choice: L.CircleDot,
+  OneLine: L.Minus,
+  Paragraph: L.Text,
   Trash: L.Trash,
+  Select: L.ChevronDown,
 };
 
 export default Icons;
