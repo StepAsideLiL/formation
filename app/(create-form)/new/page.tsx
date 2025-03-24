c5e8f2c0d90ed0f8d7079e842b3d7b86d27ae278
Rifat Khan <rifat6633@gmail.com>
2025-03-24 12:26:16 +0600

    chore: place sections in page

--- app/(create-form)/new/page.tsx
+++ app/(create-form)/new/page.tsx
@@ -1,12 +1,22 @@
-import FormEditor from "./_comps/FormEditor";
-import FormPreview from "./_comps/FormPreview";
+import AddFieldBtn from "./_comps/AddFieldBtn";
+import FormFields from "./_comps/FormFields";
 
 export default function Page() {
   return (
     <main className="mx-auto flex w-5xl items-start gap-5 p-5">
-      <FormEditor />
+      <section className="w-full space-y-5">
+        <h1 className="font-medium">Edit Form</h1>
 
-      <FormPreview />
+        <FormFields />
+
+        <div className="mx-auto w-fit">
+          <AddFieldBtn />
+        </div>
+      </section>
+
+      <section className="w-full space-y-5">
+        <h1 className="font-medium">Form Preview</h1>
+      </section>
     </main>
   );
 }
