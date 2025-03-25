4debff3502a00158332e01af0566af4e7a94f097
Rifat Khan <rifat6633@gmail.com>
2025-03-25 14:21:19 +0600

    chore: add space at the bottom.

--- app/(create-form)/new/page.tsx
+++ app/(create-form)/new/page.tsx
@@ -4,22 +4,26 @@ import FormPreview from "./_comps/FormPreview";
 
 export default function Page() {
   return (
-    <main className="mx-auto flex w-5xl items-start gap-5 p-5">
-      <section className="w-1/2 space-y-5">
-        <h1 className="font-medium">Edit Form</h1>
+    <main className="mx-auto w-5xl p-5">
+      <div className="flex items-start gap-5">
+        <section className="w-1/2 space-y-5">
+          <h1 className="font-medium">Edit Form</h1>
 
-        <FormFields />
+          <FormFields />
 
-        <div className="mx-auto w-fit">
-          <AddFieldBtn />
-        </div>
-      </section>
+          <div className="mx-auto w-fit">
+            <AddFieldBtn />
+          </div>
+        </section>
 
-      <section className="w-1/2 space-y-5">
-        <h1 className="font-medium">Form Preview</h1>
+        <section className="w-1/2 space-y-5">
+          <h1 className="font-medium">Form Preview</h1>
 
-        <FormPreview />
-      </section>
+          <FormPreview />
+        </section>
+      </div>
+
+      <div className="min-h-96"></div>
     </main>
   );
 }
