5ba16bfc9e3c9ccf341adb3f04c31b1f90af135c
Rifat Khan <rifat6633@gmail.com>
2025-03-28 14:10:54 +0600

    refactor: show field preview beside the field editor.

--- app/(create-form)/new/_comps/FormFields.tsx
+++ app/(create-form)/new/_comps/FormFields.tsx
@@ -3,15 +3,50 @@
 import atoms from "@/lib/store";
 import { useAtom } from "jotai";
 import FieldOptionsForm from "./FieldOptionsForm";
+import {
+  CheckboxField,
+  InputField,
+  RadioField,
+  SelectField,
+  TextareaField,
+} from "@/components/render-field";
 
 export default function FormFields() {
   const [formObj] = useAtom(atoms.formObjAtom);
 
   return (
     <section className="space-y-5">
-      {formObj.map((field) => (
-        <FieldOptionsForm key={field.id} options={field} />
-      ))}
+      {formObj.map((field) => {
+        return (
+          <div key={field.id} className="flex items-start gap-5">
+            <div className="w-1/2">
+              <FieldOptionsForm key={field.id} options={field} />
+            </div>
+
+            <div className="w-1/2">
+              {field.fieldType === "input" && (
+                <InputField key={field.id} field={field} />
+              )}
+
+              {field.fieldType === "textarea" && (
+                <TextareaField key={field.id} field={field} />
+              )}
+
+              {field.fieldType === "select" && (
+                <SelectField key={field.id} field={field} />
+              )}
+
+              {field.fieldType === "checkbox" && (
+                <CheckboxField key={field.id} field={field} />
+              )}
+
+              {field.fieldType === "radio" && (
+                <RadioField key={field.id} field={field} />
+              )}
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 }
--- app/(create-form)/new/page.tsx
+++ app/(create-form)/new/page.tsx
@@ -1,27 +1,14 @@
 import AddFieldBtn from "./_comps/AddFieldBtn";
 import FormFields from "./_comps/FormFields";
-import FormPreview from "./_comps/FormPreview";
 
 export default function Page() {
   return (
-    <main className="mx-auto w-5xl p-5">
-      <div className="flex items-start gap-5">
-        <section className="w-1/2 space-y-5">
-          <h1 className="font-medium">Edit Form</h1>
-
-          <FormFields />
-
-          <div className="mx-auto w-fit">
-            <AddFieldBtn />
-          </div>
-        </section>
-
-        <section className="w-1/2 space-y-5">
-          <h1 className="font-medium">Form Preview</h1>
-
-          <FormPreview />
-        </section>
-      </div>
+    <main className="p-5">
+      <section className="mx-autxo w-5xl space-y-2">
+        <h1 className="font-medium">Edit Form</h1>
+        <FormFields />
+        <AddFieldBtn />
+      </section>
 
       <div className="min-h-96"></div>
     </main>
