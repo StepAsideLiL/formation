22d089637824fbf09a3f9f895c85bf2e2e586779
Rifat Khan <rifat6633@gmail.com>
2025-03-28 14:15:34 +0600

    fix: class name.

--- app/(create-form)/new/page.tsx
+++ app/(create-form)/new/page.tsx
@@ -4,7 +4,7 @@ import FormFields from "./_comps/FormFields";
 export default function Page() {
   return (
     <main className="p-5">
-      <section className="mx-autxo w-5xl space-y-2">
+      <section className="mx-auto w-5xl space-y-2">
         <h1 className="font-medium">Edit Form</h1>
         <FormFields />
         <AddFieldBtn />
