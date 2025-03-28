3ab48c48a3ae407808346bed7e48608be810c884
Rifat Khan <rifat6633@gmail.com>
2025-03-28 23:03:39 +0600

    feat: add layout and header for home page.

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,34 @@
+import Logo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description:
+    "Formation is a form building platform for collecting data from user",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <header className="border-b py-5">
+        <div className="mx-auto flex w-5xl items-center justify-between">
+          <Logo />
+
+          <div className="flex items-center gap-2">
+            <Button className="cursor-pointer">Sign Up</Button>
+            <Button variant={"secondary"} className="cursor-pointer">
+              Sign In
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      {children}
+    </>
+  );
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import AppPreview from "./_comps/AppPreview";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <Button asChild>
-        <Link href={"/new"}>Create a New Form</Link>
-      </Button>
+    <main className="min-h-screen">
+      <section className="space-y-10">
+        <div className="mx-auto my-12 w-5xl space-y-5">
+          <h1 className="text-5xl font-medium">
+            Formation is a form building platform for collecting data from user.
+          </h1>
+
+          <Button asChild>
+            <Link href={"/new"}>Create New Form</Link>
+          </Button>
+        </div>
+
+        <AppPreview />
+      </section>
     </main>
   );
 }
