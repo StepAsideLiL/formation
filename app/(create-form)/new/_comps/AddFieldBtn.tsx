2ff49461ce21d1f0480c79423f1f07be41c4869f
Rifat Khan <rifat6633@gmail.com>
2025-04-02 09:41:56 +0600

    chore: handle insert field data by insertFormObj func.

--- app/(create-form)/new/_comps/AddFieldBtn.tsx
+++ app/(create-form)/new/_comps/AddFieldBtn.tsx
@@ -2,29 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import Icons from "@/lib/icons";
-import atoms from "@/lib/store";
-import { fieldId } from "@/lib/utils";
+import atoms, { insertFormObj } from "@/lib/store";
 import { useAtom } from "jotai";
 
 export default function AddFieldBtn() {
   const [formObj, setFormObj] = useAtom(atoms.formObjAtom);
 
   return (
-    <Button
-      className="cursor-pointer"
-      onClick={() =>
-        setFormObj([
-          ...formObj,
-          {
-            id: fieldId(),
-            label: "",
-            fieldType: "input",
-            required: true,
-            metadata: {},
-          },
-        ])
-      }
-    >
+    <Button onClick={() => setFormObj([...formObj, insertFormObj()])}>
       <Icons.Plus /> Add Field
     </Button>
   );
