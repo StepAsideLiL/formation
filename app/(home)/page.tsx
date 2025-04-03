3ef697bddec1bde3fefd5c9e62b8e14acf59a982
Rifat Khan <rifat6633@gmail.com>
2025-04-03 09:58:02 +0600

    feat: add padding.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,14 +6,16 @@ export default function Page() {
   return (
     <main className="min-h-screen">
       <section className="space-y-10">
-        <div className="mx-auto my-12 w-full max-w-5xl space-y-5">
-          <h1 className="text-center text-5xl font-medium text-balance sm:text-left">
+        <div className="mx-auto my-12 w-full max-w-5xl space-y-5 px-10">
+          <h1 className="text-center text-3xl font-medium text-balance sm:text-left sm:text-5xl">
             Formation is a form building platform for collecting data from user.
           </h1>
 
-          <Button asChild>
-            <Link href={"/new"}>Create New Form</Link>
-          </Button>
+          <div className="flex flex-col items-center justify-center gap-5 sm:flex-row sm:justify-start">
+            <Button asChild>
+              <Link href={"/new"}>Create New Form</Link>
+            </Button>
+          </div>
         </div>
 
         <AppPreview />
