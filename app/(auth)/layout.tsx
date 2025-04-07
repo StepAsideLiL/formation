ca9e019c30818ade4184599182f8dc6aadb32f57
Rifat Khan <rifat6633@gmail.com>
2025-04-07 22:46:42 +0600

    feat: add auth image.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,6 @@
+import Logo from "@/components/Logo";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +9,26 @@ export default function RootLayout({
   return (
     <>
       <main className="flex h-screen w-full">
-        <div className="w-full">Sign</div>
+        <div className="relative w-full">
+          <Image
+            src={"/img/auth.webp"}
+            alt="Auth Page Image"
+            width={1000}
+            height={1000}
+            className="w-full"
+          />
+
+          <div className="to-background from-background/50 absolute inset-0 z-10 bg-gradient-to-r"></div>
+
+          <div className="absolute inset-0 z-20 flex h-screen items-center justify-center">
+            <div className="w-xl">
+              <Logo fill="white" />
+              <h1 className="text-4xl text-balance">
+                Sign In to Formation to get the best experience
+              </h1>
+            </div>
+          </div>
+        </div>
 
         <div className="w-full">{children}</div>
       </main>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo() {
+export default function Logo({ fill = "black" }: { fill?: "black" | "white" }) {
   return (
     <Link href={"/"}>
-      <Image src={"/logo/logo-bfill.png"} alt="Logo" width={28} height={28} />
+      {fill === "black" ? (
+        <Image src={"/logo/logo-bfill.png"} alt="Logo" width={28} height={28} />
+      ) : (
+        <Image src={"/logo/logo-wfill.png"} alt="Logo" width={28} height={28} />
+      )}
     </Link>
   );
 }
