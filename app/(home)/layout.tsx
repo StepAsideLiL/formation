8e11239247811becb29b8567d93875bdd5ca83c2
Rifat Khan <rifat6633@gmail.com>
2025-04-07 14:52:12 +0600

    feat: add footer.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -29,6 +30,25 @@ export default function Layout({
       </header>
 
       {children}
+
+      <footer className="relative py-20">
+        <div className="from-foreground/10 absolute top-0 left-0 h-20 w-full bg-gradient-to-b to-transparent"></div>
+
+        <div>
+          <p className="text-foreground/50 text-center">
+            Build By{" "}
+            <Link
+              href="http://github.com/StepAsideLiL"
+              className="text-foreground underline"
+              target="_blank"
+            >
+              @StepAsideLiL
+            </Link>
+          </p>
+        </div>
+
+        <div className="from-foreground/10 absolute bottom-0 left-0 h-20 w-full bg-gradient-to-t to-transparent"></div>
+      </footer>
     </>
   );
 }
