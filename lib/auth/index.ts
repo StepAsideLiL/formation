efb297ab7fa673f2b7ca570eece4b41c8697e620
Rifat Khan <rifat6633@gmail.com>
2025-04-10 17:29:41 +0600

    feat: add username support.

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -2,6 +2,9 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prismadb";
 import { createAuthClient } from "better-auth/react";
+import { nextCookies } from "better-auth/next-js";
+import { username } from "better-auth/plugins";
+import { usernameClient } from "better-auth/client/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,8 +13,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies(), username()],
 });
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [usernameClient()],
 });
