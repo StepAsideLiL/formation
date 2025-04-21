a97a03b25e07dc1a2433f1f2af00d03882128d25
Rifat Khan <rifat6633@gmail.com>
2025-04-21 19:14:20 +0600

    feat: add loading stage in sign out button.

--- components/SignOutBtn.tsx
+++ components/SignOutBtn.tsx
@@ -2,11 +2,44 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth";
+import Icons from "@/lib/icons";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function SignOutBtn() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
   async function handleSignOut() {
-    await authClient.signOut();
+    setLoading(true);
+    const { error, data } = await authClient.signOut();
+
+    if (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+
+    if (data?.success) {
+      router.refresh();
+      setLoading(false);
+    }
   }
 
-  return <Button onClick={handleSignOut}>Sign Out</Button>;
+  return (
+    <Button disabled={loading} onClick={handleSignOut}>
+      {loading ? (
+        <>
+          <Icons.Loading
+            className="-ms-1 animate-spin"
+            size={16}
+            aria-hidden="true"
+          />
+          Signing Out
+        </>
+      ) : (
+        "Sign Out"
+      )}
+    </Button>
+  );
 }
