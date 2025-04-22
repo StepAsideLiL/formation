a2819ac8e74fcb784edcf43b01dae7ea42700af7
Rifat Khan <rifat6633@gmail.com>
2025-04-22 23:36:47 +0600

    feat: add metadata.

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,4 +1,10 @@
 import SignIn from "@/components/SignIn";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In to Formation",
+  description: "Sign in to your Formation account",
+};
 
 export default function Page() {
   return <SignIn />;
--- app/(auth)/auth/sign-up/page.tsx
+++ app/(auth)/auth/sign-up/page.tsx
@@ -1,4 +1,10 @@
 import SignUp from "@/components/SignUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create a new account",
+  description: "Create a new account in Formation.",
+};
 
 export default function Page() {
   return <SignUp />;
