03cce49bcd8442afe8a00feb9c6367e77d6a2a76
Rifat Khan <rifat6633@gmail.com>
2025-04-23 15:57:23 +0600

    feat: publish form obj to db.

---
+++ components/PublishBtn.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { publishForm } from "@/lib/actions/publishForm";
+import Icons from "@/lib/icons";
+import atoms from "@/lib/store";
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function PublishBtn({ userId }: { userId: string | undefined }) {
+  const [loading, setLoading] = useState(false);
+  const [formObj] = useAtom(atoms.formObjAtom);
+
+  async function handlePublish() {
+    if (!userId) {
+      toast.error("Sign In to Publish Form.");
+      return;
+    }
+
+    setLoading(true);
+
+    const { error, data } = await publishForm(formObj, userId);
+
+    if (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+
+    if (data) {
+      toast.success("Form Published Successfully");
+      setLoading(false);
+    }
+  }
+
+  return (
+    <Button disabled={loading} onClick={handlePublish}>
+      {loading ? (
+        <>
+          <Icons.Loading
+            className="-ms-1 animate-spin"
+            size={16}
+            aria-hidden="true"
+          />
+          Publishing
+        </>
+      ) : (
+        "Publish"
+      )}
+    </Button>
+  );
+}
--- lib/actions/publishForm.ts
+++ lib/actions/publishForm.ts
@@ -1,12 +1,36 @@
+"use server";
+
 import { TFormObj } from "@/lib/store";
 import { TResponse } from "@/lib/types";
+import prisma from "@/lib/prismadb";
 
-export async function publishForm(formObj: TFormObj[]): Promise<TResponse> {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const formObjDB = {
-    id: "1",
-    formObj: formObj,
-  };
+export async function publishForm(
+  formObj: TFormObj[],
+  userId: string,
+): Promise<TResponse> {
+  const userExists = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!userExists) {
+    return {
+      error: new Error("User not found"),
+      data: null,
+    };
+  }
+
+  const formObjDB = await prisma.formData.create({
+    data: {
+      formObj: JSON.stringify(formObj),
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
 
   return {
     error: null,
