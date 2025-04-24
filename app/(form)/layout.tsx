ba4a7558d0a43e9fff70e376b885d6e774002abd
Rifat Khan <rifat6633@gmail.com>
2025-04-24 20:18:00 +0600

    feat: add header.

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -1,7 +1,19 @@
+import Logo from "@/components/Logo";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <>
+      <header className="border-b py-5">
+        <div className="mx-auto flex w-full max-w-5xl items-center justify-center px-10 md:px-0">
+          <Logo />
+        </div>
+      </header>
+
+      {children}
+    </>
+  );
 }
