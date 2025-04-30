d96342991c0170a3fb00c83c6f9be79045384b22
Rifat Khan <rifat6633@gmail.com>
2025-04-30 21:50:48 +0600

    feat: create profile form page.

---
+++ app/(user-profile)/profile/form/[formId]/page.tsx
@@ -0,0 +1,54 @@
+import fetcher from "@/lib/fetcher";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) {
+  const form = await fetcher.getFormInfoById((await params).formId);
+
+  if (!form) {
+    return (
+      <main className="mx-auto w-full max-w-5xl py-10">
+        <h1 className="text-muted-foreground text-center text-xl">
+          Invaild Form
+        </h1>
+      </main>
+    );
+  }
+
+  return (
+    <main className="mx-auto w-full max-w-5xl space-y-10 py-5">
+      <h1 className="text-2xl font-medium">From Variants</h1>
+
+      <div>
+        {form.formSchemaVariants.map((variant) => (
+          <div key={variant.id} className="space-y-2">
+            <div className="grid grid-cols-12">
+              <span className="col-span-2">Variant Id</span>
+              <span className="col-span-10">{variant.id}</span>
+            </div>
+            <div className="grid grid-cols-12">
+              <span className="col-span-2">Create At</span>
+              <span className="col-span-10">
+                {new Date(variant.createdAt).toLocaleString()}
+              </span>
+            </div>
+            <div className="grid grid-cols-12">
+              <span className="col-span-2">Status</span>
+              <span className="col-span-10">
+                {variant.formSchema === form.formSchema
+                  ? "Current"
+                  : "Outdated"}
+              </span>
+            </div>
+            <div className="grid grid-cols-12">
+              <span className="col-span-2">Total Response</span>
+              <span className="col-span-10">{variant.formData.length}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
