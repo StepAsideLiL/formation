d48c479a407c9423da0bceec86ad7bd87d9ee883
Rifat Khan <rifat6633@gmail.com>
2025-04-30 21:49:35 +0600

    feat: design profile page.

--- app/(user-profile)/profile/page.tsx
+++ app/(user-profile)/profile/page.tsx
@@ -1,3 +1,42 @@
-export default function page() {
-  return <div>User profile page</div>;
+import fetcher from "@/lib/fetcher";
+import { Metadata, Route } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Your Profile",
+};
+
+export default async function page() {
+  const forms = await fetcher.getSignedInUserForm();
+
+  return (
+    <main className="mx-auto w-full max-w-5xl py-5">
+      {forms.error && (
+        <div>
+          <h1 className="text-muted-foreground text-center">No Form Found</h1>
+        </div>
+      )}
+
+      {forms.data && (
+        <div className="space-y-5">
+          <h1 className="text-2xl">Form List</h1>
+
+          <div>
+            {forms.data.map((form) => (
+              <Link
+                key={form.id}
+                href={`profile/form/${form.id}` as Route}
+                className="bg-background hover:bg-muted block p-5"
+              >
+                <h1 className="text-xl">{form.title}</h1>
+                <p className="text-muted-foreground text-sm">
+                  Created At {new Date(form.createdAt).toLocaleString()}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }
