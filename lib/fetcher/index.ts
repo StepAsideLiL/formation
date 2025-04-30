cae2d6667057aea575d2411aecf367b37a36052f
Rifat Khan <rifat6633@gmail.com>
2025-04-30 16:42:14 +0600

    feat: add getSignedInUserForm function.

--- lib/fetcher/index.ts
+++ lib/fetcher/index.ts
@@ -1,6 +1,8 @@
 import "server-only";
 import prisma from "@/lib/prismadb";
 import { cache } from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 const fetcher = {
   getForm: cache(async (formId: string) => {
@@ -9,6 +11,31 @@ const fetcher = {
         id: formId,
       },
     });
+  }),
+  getSignedInUserForm: cache(async () => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session) {
+      return {
+        error: {
+          message: "You are not signed in",
+        },
+        data: null,
+      };
+    }
+
+    const userForms = await prisma.form.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+    });
+
+    return {
+      error: null,
+      data: userForms,
+    };
   }),
 };
 
