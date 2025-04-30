1a1c6c11eac22b37a8f5970743f5fba7c96c2234
Rifat Khan <rifat6633@gmail.com>
2025-04-30 11:37:43 +0600

    feat: redirect to sign in page when user is not signed in while going to profile page.

--- app/(user-profile)/layout.tsx
+++ app/(user-profile)/layout.tsx
@@ -1,11 +1,22 @@
 import Logo from "@/components/Logo";
 import SignOutBtn from "@/components/SignOutBtn";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Layout({
+export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/auth/sign-in");
+  }
+
   return (
     <>
       <header className="border-b py-5">
