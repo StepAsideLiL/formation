6b679498e5a48f3f179d3f0617cc13e885888b92
Rifat Khan <rifat6633@gmail.com>
2025-05-04 09:10:57 +0600

    chore: add variant id page link to see form response.

--- app/(user-profile)/profile/form/[formId]/page.tsx
+++ app/(user-profile)/profile/form/[formId]/page.tsx
@@ -1,5 +1,6 @@
 import SetNewFormSchema from "@/components/form-app/SetNewFormSchema";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import date from "@/lib/date";
 import fetcher from "@/lib/fetcher";
 import { TFormSchema } from "@/lib/store";
@@ -47,36 +48,46 @@ export default async function Page({
 
       <div className="space-y-5">
         {form.formSchemaVariants.map((variant) => (
-          <div key={variant.id} className="space-y-2 border px-4 py-2">
-            <div className="grid grid-cols-12">
-              <span className="col-span-2">Variant Id</span>
-              <span className="col-span-10">{variant.id}</span>
+          <div key={variant.id} className="space-y-3 border px-4 py-2">
+            <div className="space-y-2">
+              <div className="grid grid-cols-12">
+                <span className="col-span-2">Variant Id</span>
+                <span className="col-span-10">{variant.id}</span>
+              </div>
+              <div className="grid grid-cols-12">
+                <span className="col-span-2">Created At</span>
+                <span className="col-span-10">
+                  {date.format(new Date(variant.createdAt))}
+                </span>
+              </div>
+              <div className="grid grid-cols-12">
+                <span className="col-span-2">Status</span>
+                <span className="col-span-10">
+                  <Badge
+                    variant={
+                      variant.formSchema === form.formSchema
+                        ? "default"
+                        : "secondary"
+                    }
+                  >
+                    {variant.formSchema === form.formSchema
+                      ? "Current"
+                      : "Outdated"}
+                  </Badge>
+                </span>
+              </div>
+              <div className="grid grid-cols-12">
+                <span className="col-span-2">Total Response</span>
+                <span className="col-span-10">{variant.formData.length}</span>
+              </div>
             </div>
-            <div className="grid grid-cols-12">
-              <span className="col-span-2">Created At</span>
-              <span className="col-span-10">
-                {date.format(new Date(variant.createdAt))}
-              </span>
-            </div>
-            <div className="grid grid-cols-12">
-              <span className="col-span-2">Status</span>
-              <span className="col-span-10">
-                <Badge
-                  variant={
-                    variant.formSchema === form.formSchema
-                      ? "default"
-                      : "secondary"
-                  }
-                >
-                  {variant.formSchema === form.formSchema
-                    ? "Current"
-                    : "Outdated"}
-                </Badge>
-              </span>
-            </div>
-            <div className="grid grid-cols-12">
-              <span className="col-span-2">Total Response</span>
-              <span className="col-span-10">{variant.formData.length}</span>
+
+            <div>
+              <Button variant={"outline"} asChild>
+                <Link href={`/profile/variant/${variant.id}` as Route}>
+                  See Responses
+                </Link>
+              </Button>
             </div>
           </div>
         ))}
