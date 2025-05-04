50a39cd35b7297056c06600ccf37af136ea73c37
Rifat Khan <rifat6633@gmail.com>
2025-05-04 23:04:07 +0600

    refactor: export TGetVariantForResponseData type.

--- lib/fetcher/getVariantForResponse.ts
+++ lib/fetcher/getVariantForResponse.ts
@@ -2,17 +2,17 @@ import { cache } from "react";
 import prisma from "@/lib/prismadb";
 import { TResponse } from "../types";
 
+export type TGetVariantForResponseData = {
+  formId: string;
+  currentVariantId: string;
+  formSchema: string;
+  formData: string[];
+};
+
 const getVariantForResponse = cache(
   async (
     variantId: string,
-  ): Promise<
-    TResponse<{
-      formId: string;
-      currentVariantId: string;
-      formSchema: string;
-      formData: string[];
-    } | null>
-  > => {
+  ): Promise<TResponse<TGetVariantForResponseData | null>> => {
     const variant = await prisma.formSchemaVariant.findUnique({
       where: {
         id: variantId,
